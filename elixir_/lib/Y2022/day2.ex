a96d31fe302ed222ea4e4f123b7ac70f248d684b
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-02-04 17:59:05 -0300

    Solving 2022, Day 2, part 2

--- elixir_/lib/Y2022/day2.ex
+++ elixir_/lib/Y2022/day2.ex
@@ -2,10 +2,20 @@ defmodule Y2022.Day2 do
   alias Common.Input
 
   def part1() do
-    Input.parse()
-    |> Enum.map(&String.split(&1, " ", trim: true))
+    parse_input()
     |> Enum.map(&parse_game(&1))
     |> Enum.sum()
+  end
+
+  def part2() do
+    parse_input()
+    |> Enum.map(&parse_game2(&1))
+    |> Enum.sum()
+  end
+
+  defp parse_input() do
+    Input.parse()
+    |> Enum.map(&String.split(&1, " ", trim: true))
   end
 
   defp parse_game([a, b]) do
@@ -26,4 +36,24 @@ defmodule Y2022.Day2 do
   defp player_winner([:scissor, :paper]), do: 0
   defp player_winner([a, b]) when a == b, do: 3
   defp player_winner([_a, _b]), do: 6
+
+  defp parse_game2([a, b]) do
+    case result = needed_paly(b) do
+      0 -> result + play_point(lose_hand(parse_play(a)))
+      3 -> result + play_point(parse_play(a))
+      6 -> result + play_point(winner_hand(parse_play(a)))
+    end
+  end
+
+  defp winner_hand(:rock), do: :paper
+  defp winner_hand(:paper), do: :scissor
+  defp winner_hand(:scissor), do: :rock
+
+  defp lose_hand(:rock), do: :scissor
+  defp lose_hand(:paper), do: :rock
+  defp lose_hand(:scissor), do: :paper
+
+  defp needed_paly("X"), do: 0
+  defp needed_paly("Y"), do: 3
+  defp needed_paly("Z"), do: 6
 end
